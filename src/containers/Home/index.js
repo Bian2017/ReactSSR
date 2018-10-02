237f0b48dca749ca013459be7a93cea028569a0c
Bian2017 <bgl_cumt_zju@163.com>
2018-10-02 10:36:38 +0800

    feat:引入Redux

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,17 +1,21 @@
 import React from 'react'   //esModule语法
 import Header from '../../components/Header'
+import { connect } from 'react-redux'
 
-const Home = () => {
+const Home = props => {
   return (
     <div>
       <Header />
-      <h1>welcome to home </h1>
-      <button onClick={() => {console.log('click')}}>
+      <h1>This is {props.name} </h1>
+      <button onClick={() => { console.log('click') }}>
         按钮
       </button>
     </div>
   )
 }
 
+const mapStateToProps = state => ({
+  name: state.name
+})
 
-export default Home
+export default connect(mapStateToProps, null)(Home)
--- src/server/utils.js
+++ src/server/utils.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { StaticRouter } from 'react-router-dom'
 import { renderToString } from 'react-dom/server'
+import { createStore, applyMiddleware } from 'redux'
 import Routes from '../Routes'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 export const serverRender = (req) => {
+  const reducer = (previousState = { name: 'Ben' }, action) => {
+    return previousState
+  }
+
+  const store = createStore(reducer, applyMiddleware(thunk))
+
   const content = renderToString((
-    <StaticRouter context={{}} location={req.path}>
-      {Routes}
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter context={{}} location={req.path}>
+        {Routes}
+      </StaticRouter>
+    </Provider>
   ))
 
   return `<html>
