09f76065c86e495099f7ac060660d6d5aaa6dfbd
Bian2017 <bgl_cumt_zju@163.com>
2018-10-02 09:31:11 +0800

    feat:Add Link

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,10 @@
 import React from 'react'   //esModule语法
+import Header from '../../components/Header'
 
 const Home = () => {
   return (
     <div>
+      <Header />
       <h1>welcome to home </h1>
       <button onClick={() => {console.log('click')}}>
         按钮
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,8 +1,10 @@
 import React from 'react'   //esModule语法
+import Header from '../../components/Header'
 
 const Login = () => {
   return (
     <div>
+      <Header />
       <h1>welcome to  Login </h1>
     </div>
   )
