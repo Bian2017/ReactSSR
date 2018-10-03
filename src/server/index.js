d955a3cca21d31a01802dd221a9948f472889a99
Bian2017 <bgl_cumt_zju@163.com>
2018-10-03 13:17:05 +0800

    feat:代码优化

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,27 @@
 import express from 'express'
+import { matchRoutes } from 'react-router-config'
 import { serverRender } from './utils'
+import getStore from '../store'
+import routes from '../Routes'
 
 const app = express()
 app.use(express.static('public'))
 
 app.get('*', function (req, res) {
-  serverRender(req, res)
+  const store = getStore()
+  // store里面填充的数据，需结合当前用户请求地址、路由做判断。
+  const matchedRoutes = matchRoutes(routes, req.path)
+
+  const promises = []
+  matchedRoutes.forEach(item => {
+    if (item.route.loadData) {
+      promises.push(item.route.loadData(store))
+    }
+  })
+
+  Promise.all(promises).then(() => {
+    res.send(serverRender({ store, routes, req }))
+  })
 })
 
 var server = app.listen(4068)
--- src/server/utils.js
+++ src/server/utils.js
@@ -1,40 +1,24 @@
 import React from 'react'
 import { StaticRouter, Route } from 'react-router-dom'
-import { matchRoutes } from 'react-router-config'
 import { renderToString } from 'react-dom/server'
-import routes from '../Routes'
 import { Provider } from 'react-redux'
-import getStore from '../store'
 
-export const serverRender = (req, res) => {
-  const store = getStore()
-
-  // store里面填充的数据，需结合当前用户请求地址、路由做判断。
-  const matchedRoutes = matchRoutes(routes, req.path)
-
-  const promises = []
-  matchedRoutes.forEach(item => {
-    if (item.route.loadData) {
-      promises.push(item.route.loadData(store))
-    }
-  })
-
+export const serverRender = ({ store, routes, req }) => {
   // 等待promises数组里的所有promise全部执行完毕，再执行下面代码
-  Promise.all(promises).then(() => {
-    const content = renderToString((
-      <Provider store={store}>
-        <StaticRouter context={{}} location={req.path}>
-          <div>
-            {
-              routes.map(route =>
-                <Route {...route} />)
-            }
-          </div>
-        </StaticRouter>
-      </Provider>
-    ))
+  const content = renderToString((
+    <Provider store={store}>
+      <StaticRouter context={{}} location={req.path}>
+        <div>
+          {
+            routes.map(route =>
+              <Route {...route} />)
+          }
+        </div>
+      </StaticRouter>
+    </Provider>
+  ))
 
-    res.send(`<html>
+  return `<html>
       <head>
         <title>服务端渲染</title>
       </head>
@@ -42,7 +26,5 @@ export const serverRender = (req, res) => {
         <div id="root">${content}</div>
         <script src='/index.js'></script>
       </body>
-    </html>`)
-
-  })
+    </html>`
 }
